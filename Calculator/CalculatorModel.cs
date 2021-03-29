6823854715dd98597c4be3e923ac41da1187f373
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-03-29 19:34:36 +0700

    удалил ненужные ссылки

--- Calculator/CalculatorModel.cs
+++ Calculator/CalculatorModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace Calculator
+﻿namespace Calculator
 {
     public class CalculatorModel
     {
