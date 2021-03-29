41b42e800841c4b5b8d555bc7938d6e069e2b992
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-03-29 19:30:22 +0700

    Добавьте файлы проекта.

---
+++ Calculator/CalculatorModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public double result { get; set; }
+        public CalculatorModel()
+        {
+
+        }
+
+        public object Calculate(double operand1, double operand2, char operation)
+        {
+            if (operation == '+')
+            {
+                result = Sum(operand1, operand2);
+            }
+
+            if (operation == '-')
+            {
+                result = Minus(operand1, operand2);
+            }
+
+            if (operation == '*')
+            {
+                result = Multiplication(operand1, operand2);
+            }
+
+            if (operation == '/')
+            {
+                result = Divide(operand1, operand2);
+            }
+
+
+            return result;
+
+
+
+        }
+
+        public double Sum(double operand1, double operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public double Minus(double operand1, double operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        public double Multiplication(double operand1, double operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        public double Divide(double operand1, double operand2)
+        {
+
+            try
+            {
+                result = operand1 / operand2;
+            }
+            catch
+            {
+                MessageBox.Show("На ноль делить нельзя!");
+            }
+            return result;
+
+
+        }
+
+
+    }
+}
